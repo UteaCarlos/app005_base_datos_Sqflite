b7179d940b24d8b5e7587b9b7190d025aeb54b2f
Carlos Huanca <202000203c@utea.edu.pe>
2022-11-14 16:06:42 -0500

    first commit

---
+++ lib/db/db_admin.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBAdmin {
+  Database? myDatabase;
+
+  static final DBAdmin db = DBAdmin._();
+  DBAdmin._();
+
+  Future<Database?> chekDatabase() async {
+    if (myDatabase != null) {
+      return myDatabase;
+    }
+
+    myDatabase = await initDatabase();
+    return myDatabase;
+  }
+
+  Future<Database> initDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "Cliente.db");
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database dbx, int version) async {
+        await dbx.execute(
+            "CREATE TABLE Cliente(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT,Apellidos TEXT, Direccion TEXT, Telefono TEXT,Correo TEXT)");
+      },
+    );
+  }
+}
