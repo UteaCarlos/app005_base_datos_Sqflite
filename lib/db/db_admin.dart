c27081520aa60f1c04144d646e27c8f945699e1c
Carlos Huanca <202000203c@utea.edu.pe>
2022-11-24 23:50:45 -0500

    commit_BaseDatos_MostrarDatos

--- lib/db/db_admin.dart
+++ lib/db/db_admin.dart
@@ -27,9 +27,83 @@ class DBAdmin {
       version: 1,
       onOpen: (db) {},
       onCreate: (Database dbx, int version) async {
+        //CREAR LA TABLA DATOS CLIENTE
         await dbx.execute(
             "CREATE TABLE Cliente(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT,Apellidos TEXT, Direccion TEXT, Telefono TEXT,Correo TEXT)");
       },
     );
+  }
+
+  // INSERTAR DATOS A LA TABLA CLIENTE inserRawtTask
+  inserRawtTask() async {
+    Database? db = await chekDatabase();
+    int res = await db!.rawInsert(
+        "INSERT INTO Cliente(Nombre, Apellidos, Direccion,Telefono,Correo)VALUES ('Carlos','Huanca','Av. Pardo','984709285','202000203c@utea.edu.pe')");
+    print(res);
+  }
+
+  // INSERTAR DATOS A LA TABLA CLIENTE insertTask
+
+  insertTask() async {
+    Database? db = await chekDatabase();
+    int res = await db!.insert(
+      "Cliente",
+      {
+        "Nombre": "Elizabeth",
+        "Apellidos": "Rojas",
+        "Direccion": "Av. La Cultura",
+        "Telefono": "984752685",
+        "Correo": "rojas@gmail.com",
+      },
+    );
+    print(res);
+  }
+
+  getRawTasks() async {
+    Database? db = await chekDatabase();
+    List tasks = await db!.rawQuery("SELECT * FROM Cliente");
+    print(tasks[0]);
+  }
+
+  Future<List<Map<String, dynamic>>> getTasks() async {
+    Database? db = await chekDatabase();
+    List<Map<String, dynamic>> tasks = await db!.query("Cliente");
+    return tasks;
+  }
+
+  //PARA ACTUALIZAR DATOS
+  updateRawTask() async {
+    Database? db = await chekDatabase();
+    int res = await db!.rawUpdate(
+        "UPDATE Cliente SET Nombre = 'Jorge', Apellidos = 'Ramos', Direccion = 'Av. Sol', Telefono = '123456789', Correo = 'Jorge@gmail.com' WHERE id =2");
+    print(res);
+  }
+//PARA ACTUALIZAR DATOS MAS DIRECTOS
+  updateTask() async {
+    Database? db = await chekDatabase();
+    int res = await db!.update(
+      "Cliente",
+      {
+       "Nombre":"Juan Carlos",
+        "Apellidos":"Flores",
+        "Direccion":"Wanchaq",
+        "Telefono":"987456321",
+        "Correo":"juancarlos@gmail.com",
+      },
+      where: "id = 2"
+    );
+  }
+//PARA ELIMINAR UN REGISTRO
+    deleteRawTask() async {
+      Database? db = await chekDatabase();
+      int res = await db!.rawDelete("DELETE FROM Cliente WHERE id = 2");
+      print(res);
+
+  }
+
+  deleteTask() async {
+    Database? db = await chekDatabase();
+   int res = await db!.delete("Cliente",where: "id = 11" );
+   print(res);
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,25 +2,62 @@ import 'package:app05_basedatos/db/db_admin.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  //const HomePage({super.key});
+  Future <String> getFullName() async{
+    return "Juan Mauel";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text("HomePage"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                DBAdmin.db.initDatabase();
-              },
-              child: Text("Mostrar data"),
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: DBAdmin.db.getTasks(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if (snap.hasData){
+            List
+          }
+          print(snap.data);
+          return Text("Hola");
+        },
+        //child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //children: [
+            //ElevatedButton(
+             // onPressed: () {
+
+               // DBAdmin.db.updateTask();
+             // },
+             // child: Text("Mostrar data"),
+            //),
+           // ElevatedButton(
+             // onPressed: () {
+
+               // DBAdmin.db.insertTask();
+
+             // },
+             // child: Text("Insertar Tarea"),
+            //),
+
+           // ElevatedButton(
+             // onPressed: () {
+
+              //  DBAdmin.db.updateTask();
+             // },
+             // child: Text("Actualizar Tarea"),
+            //),
+
+           // ElevatedButton(
+             // onPressed: () {
+
+               // DBAdmin.db.deleteTask();
+             // },
+             // child: Text("Eliminar Tarea"),
+            //),
+          //],
+        //),
       ),
     );
   }
